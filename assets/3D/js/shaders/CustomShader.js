0bfb91f7e4945aad715062bd03b56d594ff2ca87
Jan Vorisek <vorisek.jan@outlook.com>
2019-06-29 22:27:47 +0200

    Header update.

--- assets/3D/js/shaders/CustomShader.js
+++ assets/3D/js/shaders/CustomShader.js
@@ -11,14 +11,16 @@ var frag = `
 	uniform vec2 tSize;
 	uniform sampler2D tDiffuse;
 	uniform sampler2D tBayer;
+	uniform vec4 blend;
 	varying vec2 vUv;
 	vec4 dither(vec4 col, vec2 frag){    
 		float nColR = 3.;
 		float nColG = 3.;
-		float nColB = 3.;							
-		vec3 downsampled = vec3(floor((col.r + texture2D(tBayer, mod(frag / 8., 1.)).r / nColR) * nColR) / nColR,
-								floor((col.g + texture2D(tBayer, mod(frag / 8., 1.)).r / nColG) * nColG) / nColG,
-								floor((col.b + texture2D(tBayer, mod(frag / 8., 1.)).r / nColB) * nColB) / nColB); 
+		float nColB = 3.;					
+		vec4 texel = texture2D(tBayer, mod(frag / 8., 1.));	
+		vec3 downsampled = vec3(floor((col.r + texel.r / nColR) * nColR) / nColR,
+								floor((col.g + texel.r / nColG) * nColG) / nColG,
+								floor((col.b + texel.r / nColB) * nColB) / nColB); 
 		return vec4((downsampled.rgb ), col.a);
 	}
 
@@ -31,7 +33,7 @@ var frag = `
 		float t = (.1 * sin(uv.x * 10. + time) + (.05 * sin(uv.x * 5. + time)) - (.025 * cos(uv.x * 20. - time * 5. + .2)) + (.005 * sin(uv.x * 50. - time * 5. + .4))) * .5 - .05;
 		color *= 1. - 1.5 * smoothstep(.1 + t, .01 + t, uv.y);
 		color = mix(color, vec4(vec3(1., 1., 1.) * (color.r + color.b + color.r) / 3., 1.), .75);
-		return dither(color * vec4(.95, .86, .8, 1.), frag);
+		return dither(color * vec4(.95, .86, .8, 1.) * blend, frag);
 	}
 
 	void main() {		
@@ -53,16 +55,14 @@ THREE.BktCustom = {
 	uniforms: {
 		"tBayer": { value: null },
 		"tDiffuse": { value: null },
-		"tSize":    { value: new THREE.Vector2( 256, 256 ) },
-		"center":   { value: new THREE.Vector2( 0.5, 0.5 ) },
-		"angle":    { value: 1.57 },
-		"time": 	{ value: 0.},
-		"scale":    { value: 1.0 }
-
+		"tSize": { value: [256, 256] },
+		"center": { value: [0.5, 0.5] },
+		"angle": { value: 1.57 },
+		"time": { value: 0. },
+		"scale": { value: 1.0 },
+		"blend": { value: [1., 1., 1., 1.] }
 	},
 
 	vertexShader: vert,
-
 	fragmentShader: frag
-
 };
--- assets/3D/main.js
+++ assets/3D/main.js
@@ -1,8 +1,17 @@
 
-var model, timer, scene, camera, w, h, renderer, light, loader, bayerTexture, effect;
+var model, timer, scene, camera, w, h, renderer, light, loader, bayerTexture, effect, rand, fade, zoom;
 w = window.innerWidth;
 h = window.innerHeight;
+fade = 0.0;
+rand = Math.random() * 6.28;
+zoom = 150.0 + Math.floor(Math.random() * 3.0) * 25.0;
 init();
+animate();
+
+function mouseMoved( evt ) {
+    console.log("move");    
+}
+
 function init() {
     renderer = new THREE.WebGLRenderer({ canvas: headerCanvas, antialias: true, alpha: true });
     renderer.setSize(w, h);
@@ -11,7 +20,7 @@ function init() {
     scene = new THREE.Scene();
     renderer.setClearColor(0x000000, 0);
 
-    camera = new THREE.OrthographicCamera(w / - 200, w / 200, h / 200, h / - 200, 1, 1000);
+    camera = new THREE.OrthographicCamera(w / - zoom, w / zoom, h / zoom, h / - zoom, 1, 1000);
     timer = new THREE.Clock(true);
     loader = new THREE.GLTFLoader();
 
@@ -32,6 +41,8 @@ function init() {
         console.error(error);
     });
 
+
+
     light = new THREE.PointLight(0x7A2020, 5, 5);
     light.castShadow = true;
     light.position.set(2.91251, 4.2293, -0.003803);
@@ -48,8 +59,8 @@ function init() {
     bayerTexture.magFilter = THREE.NearestFilter;
     bayerTexture.minFilter = THREE.NearestFilter;
 
-    effect = new THREE.ShaderPass(THREE.BktCustom);
-    effect.uniforms['scale'].value = 2;
+    effect = new THREE.ShaderPass(THREE.BktCustom);    
+    effect.uniforms['scale'].value = 2;    
     effect.uniforms['tBayer'].value = bayerTexture;
     effect.uniforms['time'].value = timer.getElapsedTime();
 
@@ -59,28 +70,34 @@ function init() {
 }
 function onWindowResize() {
     w = window.innerWidth;
-    h = window.innerHeight;
-    camera.left = w / -200;
-    camera.right = w / 200
-    camera.top = h / 200;
-    camera.bottom = h / - 200;
+    h = window.innerHeight;      
+    camera.left = w / -zoom;
+    camera.right = w / zoom
+    camera.top = h / zoom;
+    camera.bottom = h / - zoom;
     //camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     composer.setSize(window.innerWidth, window.innerHeight);
 }
 
+
+
+
 function animate() {
     requestAnimationFrame(animate);
 
-    //model.rotation.y = -Math.PI / 2. + .1;
     if (typeof model !== 'undefined') {
-        model.rotation.y = .05 + Math.sin(timer.getElapsedTime() * .5 - 1.5) * .5;
+        model.rotation.y = .05 + Math.sin(timer.getElapsedTime() * .5 - 1.5 + rand) * .5;
         model.rotation.z = .25 * Math.sin(timer.getElapsedTime() * 1.);
         model.position.x = 0.027778;
         model.position.z = 0.111111;
-        model.position.y = 0.887044;
+        model.position.y = 0.887044 + Math.sin(timer.getElapsedTime() * 1.1 + .6) * .5;      
+        model.scale.z = 2 - fade;  
+        model.scale.y = .5 + fade * .5;  
+        fade = Math.min(1.0, fade + (1 - fade) * .05);
     }
+
     camera.position.x = 4.05667;
     camera.position.y = 3.92538;
     camera.position.z = .411046;
@@ -88,10 +105,11 @@ function animate() {
     camera.rotation.x = .05;
     camera.rotation.y = Math.PI / 2 - .1;    
     camera.rotation.z = .08;
-    
+     
+    effect.uniforms['blend'].value = [1.05 * fade, 1.0 * fade, 1.1 * fade, 1.0 * fade];
     effect.uniforms['tSize'].value = [window.innerWidth, window.innerHeight];
     effect.uniforms['time'].value = timer.getElapsedTime();
     composer.render();
     //renderer.render(scene, camera);
 }
-animate();
+
